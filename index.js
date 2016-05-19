c325d44ed68dedfd8aa5e46560a795f73779cd87
Peter Jankovsky <peter@littlepassports.com>
2016-05-19 14:03:43 -0600

    use lib instead of direct calls

--- index.js
+++ index.js
@@ -1,41 +1,45 @@
 'use strict';
-let aws = require('aws-sdk');
-let dynamodb = new aws.DynamoDB();
+const tgConfig = require('tg-config');
 
 exports.handler = function(event, context, callback) {
+    var res;
     if (typeof event.key === 'undefined') {
         // grab the whole table
-        dynamodb.scan({
-            TableName: 'TokenGoodsConfig'
-        }, function(err, data){
-            if (err)
-                callback(err);
-            else {
-                var config = {};
-                for (var i=0; i < data.Items.length; i++) {
-                    config[data.Items[i].key.S] = data.Items[i].value.S;
-                }
-                callback(null, config);
-            }
-        });
+        res = tgConfig.getAllConfig();
+        callback(res[0], res[1]);
+        // dynamodb.scan({
+        //     TableName: 'TokenGoodsConfig'
+        // }, function(err, data){
+        //     if (err)
+        //         callback(err);
+        //     else {
+        //         var config = {};
+        //         for (var i=0; i < data.Items.length; i++) {
+        //             config[data.Items[i].key.S] = data.Items[i].value.S;
+        //         }
+        //         callback(null, config);
+        //     }
+        // });
     } else {
         // grab just a single key
-        dynamodb.getItem({
-            Key: {
-                key: {
-                    S: String(event.key)
-                }
-            },
-            TableName: 'TokenGoodsConfig'
-        }, function(err, data){
-            if (err)
-                callback(err);
-            else {
-                if (typeof data.Item === 'undefined')
-                    callback('Config value not found');
-                else
-                    callback(null, data.Item.value.S);
-            }
+        res = tgConfig.getConfig(event.key);
+        callback(res[0], res[1]);
+        // dynamodb.getItem({
+        //     Key: {
+        //         key: {
+        //             S: String(event.key)
+        //         }
+        //     },
+        //     TableName: 'TokenGoodsConfig'
+        // }, function(err, data){
+        //     if (err)
+        //         callback(err);
+        //     else {
+        //         if (typeof data.Item === 'undefined')
+        //             callback('Config value not found');
+        //         else
+        //             callback(null, data.Item.value.S);
+        //     }
         });
     }
 };
