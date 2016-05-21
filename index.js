c0ad90666fe2debcafb6a47cdf739dd409e00390
Peter Jankovsky <peter@littlepassports.com>
2016-05-21 09:34:45 -0600

    use new config lib name

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 'use strict';
-let tgConfig = require('tg-node-lib/lib/tg-config');
+let config = require('tg-node-lib/lib/config');
 
 exports.handler = function(event, context, callback) {
     if (typeof event.key === 'undefined') {
         // grab the whole table
-        tgConfig.getAllConfig()
+        config.getAllConfig()
             .then((config) => callback(null, config))
             .catch((err) => callback(err, null));
     } else {
         // grab just a single key
-        tgConfig.getConfig(event.key)
+        config.getConfig(event.key)
             .then((value) => callback(null, value))
             .catch((err) => callback(err, null));
     }
